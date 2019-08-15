a0fd66967e36bb6e88d376b1c3ae7a755c022ef1
Paul Bloomfield <paulbloomfield.uk@gmail.com>
2019-08-15 21:24:46 +0100

    FIx install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,11 +8,16 @@ sudo apt autoremove -y
 # Install dependencies
 sudo apt install -y nodejs npm
 
+git config --global user.email "pi@example.com"
+get config --global user.name "Raspberry Pi"
+
 BRANCH="master"
 
 ## Set the branch if necessary.
 if [ "$1" != "" ]; then
     BRANCH="$1"
+    git checkout master
+    git branch -d $BRANCH
 fi
 
 git fetch origin $BRANCH
